a236126ec906fa2c8a639d96c6be8140c68785e0
Jardel Back Kuhnen <jardelkuhnen@gmail.com>
2019-05-20 20:07:25 -0300

    Adicionado crud produto.

--- src/main/java/br/com/seniorSistemas/controllers/ProductController.java
+++ src/main/java/br/com/seniorSistemas/controllers/ProductController.java
@@ -1,10 +1,93 @@
 package br.com.seniorSistemas.controllers;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.seniorSistemas.dto.ProductDTO;
+import br.com.seniorSistemas.entities.Product;
+import br.com.seniorSistemas.services.ProductService;
+import br.com.seniorSistemas.utils.Response;
+
 @RestController
-@RequestMapping("api/v1/product")
+@RequestMapping("api/product")
 public class ProductController {
 
+	@Autowired
+	private ProductService productService;
+
+	@GetMapping("/listAll")
+	public List<Product> listAll() {
+		return productService.listAll();
+	}
+
+	@GetMapping("/loadById/{id}")
+	public ResponseEntity<?> loadById(@PathVariable Long id) {
+		Response<?> response = new Response<>();
+
+		if (id == null) {
+			response.getErrors().add("Cliente não localizado");
+			return ResponseEntity.badRequest().body(response);
+		}
+
+		Product productMatched = productService.loadById(id);
+
+		return ResponseEntity.ok(productMatched);
+	}
+
+	@PostMapping("/save")
+	public ResponseEntity<Response<ProductDTO>> save(@Valid @RequestBody ProductDTO productDTO, BindingResult result) {
+
+		Response<ProductDTO> response = new Response<ProductDTO>();
+
+		if (result.hasErrors()) {
+			result.getAllErrors().forEach(error -> response.getErrors().add(error.getDefaultMessage()));
+			return ResponseEntity.badRequest().body(response);
+		}
+
+		productService.save(productDTO);
+
+		return ResponseEntity.ok().build();
+	}
+
+	@PostMapping("/update")
+	public ResponseEntity<Response<ProductDTO>> update(@Valid @RequestBody ProductDTO productDTO, BindingResult result) {
+		Response<ProductDTO> response = new Response<ProductDTO>();
+
+		if (result.hasErrors()) {
+			result.getAllErrors().forEach(error -> response.getErrors().add(error.getDefaultMessage()));
+			return ResponseEntity.badRequest().body(response);
+		}
+		
+		productService.update(productDTO);
+
+		return ResponseEntity.ok().build();
+	}
+	
+	@DeleteMapping("delete/{id}")
+	public ResponseEntity<?> delete(@PathVariable Long id){
+		Response<?> response = new Response<>();
+		
+		if (id == null) {
+			response.getErrors().add("Produto não localizado");
+			return ResponseEntity.badRequest().body(response);
+		}
+		
+		productService.delete(id);
+		
+		return ResponseEntity.ok().build();
+		
+	}
+	
 }
--- src/main/java/br/com/seniorSistemas/services/ProductService.java
+++ src/main/java/br/com/seniorSistemas/services/ProductService.java
@@ -1,8 +1,56 @@
 package br.com.seniorSistemas.services;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import br.com.seniorSistemas.dto.ProductDTO;
+import br.com.seniorSistemas.entities.Product;
+import br.com.seniorSistemas.repositories.ProductRepository;
 
 @Service
 public class ProductService {
+	
+	@Autowired
+	private ProductRepository productRepository;
+	
+	public List<Product> listAll() {
+		return productRepository.findAll();
+	}
+
+	public Product loadById(Long id) {
+		return productRepository.findById(id).get();
+	}
+
+	private void setInformation(ProductDTO productDTO, Product product) {
+		product.setDescription(productDTO.getDescription());
+		product.setPriceBuy(productDTO.getPriceBuy());
+		product.setPriceSale(productDTO.getPriceSale());
+	}
+
+	public void save(@Valid ProductDTO productDTO) {
+		
+		Product product = new Product();
+		setInformation(productDTO, product);
+		
+		productRepository.save(product);
+	}
+
+	public void update(@Valid ProductDTO productDTO) {
+		
+		Product product = productRepository.findById(productDTO.getId()).get();
+		setInformation(productDTO, product);
+
+		productRepository.save(product);
+	}
+
+	public void delete(Long id) {
+		Product product = productRepository.findById(id).get();
+		productRepository.delete(product);
+		
+	}
 
 }
